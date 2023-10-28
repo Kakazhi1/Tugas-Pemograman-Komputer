fc764d0a41b0a8a9e53d809896c342cd72107983
Kakazhi1 <148599432+Kakazhi1@users.noreply.github.com>
2023-10-28 17:19:12 +0700

    Tugas Pemograman Komputer 4.2

---
+++ Tugas 4 2.m
@@ -0,0 +1,49 @@
+>> %Tugas 4.2
+>> 
+>> %a. Matrik identitas dengan dimensi 5x5
+>> A = eye (5)
+
+A =
+
+     1     0     0     0     0
+     0     1     0     0     0
+     0     0     1     0     0
+     0     0     0     1     0
+     0     0     0     0     1
+
+>> %b. Matrik dengan dimensi 3x3 dengan elemen-elemen semuanya bernilai nol
+>> B = zeros (3,3)
+
+B =
+
+     0     0     0
+     0     0     0
+     0     0     0
+
+>> %c. Matrik dengan dimensi 4x4 dengan elemen-elemen semuanya bernilai satu
+>> C = ones (4,4)
+
+C =
+
+     1     1     1     1
+     1     1     1     1
+     1     1     1     1
+     1     1     1     1
+
+>> %d. Matrik dengan dimensi 3x4 dimana elemen-elemen terdistribusi antara 0 dan 1 (default n=m)
+>> D = rand (3,4)
+
+D =
+
+    0.8147    0.9134    0.2785    0.9649
+    0.9058    0.6324    0.5469    0.1576
+    0.1270    0.0975    0.9575    0.9706
+
+>> %e. Matrik dengan dimensi 3x4 dimana elemen-elemen terdistribusi normal
+>> E = randn (3,4)
+
+E =
+
+    0.7254   -0.2050    1.4090   -1.2075
+   -0.0631   -0.1241    1.4172    0.7172
+    0.7147    1.4897    0.6715    1.6302
